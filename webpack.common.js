e9e9e9998e9c0e5cb14d005e8746c4d107bf4937
Daniel Gallardo <dgallardo.c@outlook.com>
2023-03-08 19:14:28 -0600

    Add template for entry point

--- webpack.common.js
+++ webpack.common.js
@@ -3,7 +3,9 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
-    plugins: [new HtmlWebpackPlugin()],
+  plugins: [new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })],
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
