ae38d07d40ea581a5be094e427b2ea8101332cee
Daniel Gallardo <dgallardo.c@outlook.com>
2023-03-09 22:21:42 -0600

    Split and display first word on element

--- src/components/word-viewer.js
+++ src/components/word-viewer.js
@@ -3,16 +3,20 @@ import { html, LitElement } from "lit";
 class WordViewer extends LitElement {
   static properties = {
     words: {},
+    idx: { state: true },
   }
 
   constructor() {
     super();
     this.words = 'Initial value!';
+    this.idx = 0;
   }
 
   render() {
+    const splittedWords = this.words.split('.');
+    const actualWord = splittedWords[this.idx % splittedWords.length];
     return html`
-    <pre>${this.words}</pre>
+    <pre>${actualWord}</pre>
     `;
   }
 }
