39415867ed82c7a285ee3379b79202dfebd6d73e
Daniel Gallardo <dgallardo.c@outlook.com>
2023-03-09 20:49:16 -0600

    Add header to page

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import { LitElement, html } from "lit";
+import "./components/header.js";
 
 class MyApp extends LitElement {
   render() {
     return html`
+    <my-header></my-header>
     <p>Hello Lit!</p>
     `
   }
