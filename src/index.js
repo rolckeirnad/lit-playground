9cf753cc802305592ab5b1e7af68fd632ec9d224
Daniel Gallardo <dgallardo.c@outlook.com>
2023-03-10 17:44:26 -0600

    Add motion-carousel with external images

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { LitElement, html } from "lit";
 import "./components/header.js";
 import "./components/word-viewer.js";
+import "./components/carousel.js";
 
 class MyApp extends LitElement {
   render() {
@@ -8,6 +9,15 @@ class MyApp extends LitElement {
     <my-header></my-header>
     <p>Hello Lit!</p>
     <word-viewer words="👋.from.html"></word-viewer>
+    <h2>motion-carousel</h2>
+    <motion-carousel id="carousel">
+      <img src="https://picsum.photos/seed/1/200">
+      <img src="https://picsum.photos/seed/2/200">
+      <img src="https://picsum.photos/seed/3/200">
+      <img src="https://picsum.photos/seed/4/200">
+      <img src="https://picsum.photos/seed/5/200">
+      <img src="https://picsum.photos/seed/6/200">
+    </motion-carousel>
     `
   }
 }
