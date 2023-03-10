eefc617263fc310630590edec35266634e2a96fd
Daniel Gallardo <dgallardo.c@outlook.com>
2023-03-09 22:17:04 -0600

    Display word viewer component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import { LitElement, html } from "lit";
 import "./components/header.js";
+import "./components/word-viewer.js";
 
 class MyApp extends LitElement {
   render() {
     return html`
     <my-header></my-header>
     <p>Hello Lit!</p>
+    <word-viewer words="👋.from.html"></word-viewer>
     `
   }
 }
